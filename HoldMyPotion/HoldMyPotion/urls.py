af013856db257fb8bca99d5c846fa9e3ab725582
Elijah <rahulsharma240301@gmail.com>
2020-03-29 00:37:50 +0530

    Setup a Basic signin form and login form

--- HoldMyPotion/HoldMyPotion/urls.py
+++ HoldMyPotion/HoldMyPotion/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('about/', views.about_page, name='about_page'),
     path('articles/', include('app.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
