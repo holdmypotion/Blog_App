6534c31c38c4e2d11d4bea4ec9463609acb6ba2e
Elijah <rahulsharma240301@gmail.com>
2020-03-28 23:14:32 +0530

    Added Thumbnail feature to the articles.

--- HoldMyPotion/HoldMyPotion/urls.py
+++ HoldMyPotion/HoldMyPotion/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+
+from django.conf import settings
 
 from . import views
 
@@ -26,4 +29,5 @@ urlpatterns = [
     path('articles/', include('app.urls')),
 ]
 
-urlpatterns += staticfiles_urlpatterns() 
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
